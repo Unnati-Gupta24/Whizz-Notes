126d443abbd407b15bee7ec8dc47ae5e437530eb
Unnati gupta <guptaunnati031@gmail.com>
2024-08-09 21:55:00 +0530

    Update note.js

--- note.js
+++ note.js
@@ -14,6 +14,12 @@ app.get("/",(req,res)=>{
     })
 });
 
+app.get("/file/:filename",(req,res)=>{
+    fs.readFile(`./files/${req.params.filename}`,"utf-8",function(err,filedata){
+        res.render("show",{filename: req.params.filename,filedata});
+    });
+});
+
 app.post("/create",(req,res)=>{
     fs.writeFile(`./files/${req.body.title.split(' ').join('')}`, req.body.details, function(err){
         res.redirect("/")
